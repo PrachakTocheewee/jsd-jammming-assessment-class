1ed86640d11bf8b04a294116f49498ed3d1c0dac
Prachak Tocheewee <prachak.toc@gmail.com>
2022-07-20 19:28:07 +0700

    Edit TrackList Again

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,28 +14,28 @@ class App extends React.Component {
 
     this.state = {
       searchResults: [
-        { name: "name1", artist: "artist1", album: "album1", id: 1 },
-        { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
+        { name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
+        { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
+        { name: 'name3', artist: 'artist3', album: 'album3', id: 3 },
       ],
       playlistName: "My Playlist",
       playlistTracks: [
         {
-          name: "playlistName1",
-          artist: "playlistArtist1",
-          album: "playlistAlbum1",
+          name: 'playlistName1',
+          artist: 'playlistArtist1',
+          album: 'playlistAlbum1',
           id: 4,
         },
         {
-          name: "playlistName2",
-          artist: "playlistArtis2",
-          album: "playlistAlbum2",
+          name: 'playlistName2',
+          artist: 'playlistArtis2',
+          album: 'playlistAlbum2',
           id: 5,
         },
         {
-          name: "playlistName3",
-          artist: "playlistArtist3",
-          album: "playlistAlbum3",
+          name: 'playlistName3',
+          artist: 'playlistArtist3',
+          album: 'playlistAlbum3',
           id: 6,
         },
       ],
--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -3,17 +3,20 @@ import React, { Component } from "react";
 //import TrackList.css
 import "./TrackList.css";
 
-//import Track from "../Track/Track";
+import Track from "../Track/Track";
 
 class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {this.props.tracks.map(track => {
-          return <Track track={track} key={track.id} />
-        })}
+        {
+          this.props.tracks.map(track => {
+          return <Track track={track} 
+          key={track.id} />
+        })
+        }
       </div>
-    );
+    )
   }
 }
 
