ac303967f60c1ee3172932830987c377abaef99d
Prachak Tocheewee <prachak.toc@gmail.com>
2022-07-20 21:31:05 +0700

    task47

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -9,15 +9,14 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {
-            this.props.tracks.map(track => {
-            return <Track track={track} 
-            key={track.id} />
-            })
-        }
+        {this.props.tracks.map((track) => {
+          return (
+            <Track track={track} key={track.id} onAdd={this.props.onAdd} />
+          );
+        })}
       </div>
-    )
-  }                                                          
+    );
+  }
 }
 
 export default TrackList;
